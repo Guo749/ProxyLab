64bf88bc128a06b24ffb66dfc293c672fcb71b19
Wenchao Guo <wenchao.guo@duke.edu>
2022-01-02 21:02:10 +0000

    one bug: no caching used part, line 98

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -1,18 +1,24 @@
 #include "csapp.h"
 #include "sbuf.h"
-
+#include "cache.h"
 
 /* Recommended max cache and object sizes */
-#define MAX_CACHE_SIZE 1049000
-#define MAX_OBJECT_SIZE 102400
 #define WEB_PREFIX "http://"
 #define NTHREADS 4
 #define SBUFSIZE 16
+#define NO_EXIST_IN_CACHE 0
+#define EXIST_IN_CACHE 1
+#define CR "\r\n"
+#define READER_SAME_TIME 3
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-sbuf_t sbuf; /* Shared buffer of connected descriptors */
+static sbuf_t sbuf; /* Shared buffer of connected descriptors */
+static cache_t cache;
+static int cacheSize; /* gloabl variable to check the cache size */
+static int readcnt;
+static sem_t mutex, W;
 
 void* thread(void* vargp);
 void handleRequest(int);
@@ -21,6 +27,9 @@ int readAndFormatRequestHeader(rio_t* , char* , char*, char* , char* , char* , c
 int checkGetMethod(char* , char* , char* );
 void replaceHTTPVersion(char* );
 void parseLine(char* , char*, char* , char* , char* , char*, char*);
+void readAndWriteResponse(int , rio_t*, char* uri);
+void writeToCache(obj_t* );
+int readItem(char* , int );
 
 void handleRequest(int fd){
     /* the argument used to parse the first line of http request */
@@ -60,9 +69,9 @@ void handleRequest(int fd){
         return;
     }
     
-    printf("========= we have formatted the reqeust into ---------\n");
+    printf("========= $$BEGIN we have formatted the reqeust into $$=========\n");
     printf("%s", clientRequest);
-
+    printf("========= $$END we have formatted the reqeust into $$=========\n");
     /** step3: establish own connection with tiny server
      *         and forward the request to the client
      * localhost:1025
@@ -70,8 +79,6 @@ void handleRequest(int fd){
     char hostName[100];
     char* colon = strstr(host, ":");
     strncpy(hostName, host, colon - host);
-    printf("host is %s\n", hostName);
-    printf("port is %s\n", port);
     int clientfd = Open_clientfd(hostName, port);
 
     Rio_readinitb(&rioTiny, clientfd);
@@ -79,12 +86,68 @@ void handleRequest(int fd){
 
     /** step4: read the response from tiny and send it to the client */
     printf("---prepare to get the response---- \n");
-    char tinyResponse[MAXLINE];
+    // int n; char tinyResponse[MAXLINE];
+    // while( (n = Rio_readlineb(&rioTiny, tinyResponse, MAXLINE)) != 0){
+    //     Rio_writen(fd, tinyResponse, n);
+    // }
+    readAndWriteResponse(fd, &rioTiny, uri);
+}
 
-    int n;
-    while( (n = Rio_readnb(&rioTiny, tinyResponse, MAXLINE)) != 0){
+void readAndWriteResponse(int fd, rio_t* rioTiny, char* uri){
+    // int rv = readItem(uri, fd);
+    // if(rv != 0){ // we send it using cache
+    //     printf("======we are using cache to send the response back ====\n");
+    //     return;
+    // }
+
+    char tinyResponse[MAXLINE];
+    int n, totalBytes = 0;
+    //new response
+    obj_t* obj = Malloc(sizeof(*obj));
+    obj->flag = '0';
+    strcpy(obj->uri, uri);
+    *obj->respHeader = 0;
+    *obj->respBody   = 0;
+
+    printf("obj->url == %s\n", obj->uri);
+
+    while( (n = rio_readlineb(rioTiny, tinyResponse, MAXLINE)) != 0){
         Rio_writen(fd, tinyResponse, n);
+
+        if(strcmp(tinyResponse, "\r\n") == 0) // prepare for body part
+            break;
+
+        strcat(obj->respHeader, tinyResponse);
+        totalBytes += n;
     }
+
+    obj->respHeaderLen = totalBytes;
+    totalBytes = 0;
+
+    while( (n = rio_readlineb(rioTiny, tinyResponse, MAXLINE)) != 0){
+        Rio_writen(fd, tinyResponse, n);
+        totalBytes += n;
+        strcat(obj->respBody, tinyResponse);
+    }
+
+    obj->respBodyLen = totalBytes;
+    //check if the cache is too large
+    if(totalBytes >= MAX_OBJECT_SIZE){
+        Free(obj);
+        return;
+    }
+
+    printf("In reading Responsse, we have cache the follwing item\n");
+    printf("======= response header ========\n");
+    printf("%s", obj->respHeader);
+    printf("======= response body ==========\n");
+    printf("%s", obj->respBody);
+
+    /* try to read current capacity and write into it */
+    P(&W);
+    writeToCache(obj);
+    printf("=======writng this object in to cache=========\n");
+    V(&W);
 }
 
 
@@ -98,12 +161,9 @@ int readAndFormatRequestHeader(rio_t* rio, char* clientRequest, char* Host, char
     sprintf(clientRequest, "GET %s HTTP/1.0\r\n", fileName);
 
     n = Rio_readlineb(rio, buf, MAXLINE);
-    printf("receive buf %s\n", buf);
-    printf("n == %d\n", n);
     char* findp;
     while(strcmp("\r\n", buf) != 0 && n != 0){
         strcat(clientRequest, buf);
-        printf("receive buf %s\n", buf);
 
         if( (findp = strstr(buf, "User-Agent:")) != NULL){
             UserAgent = 1;
@@ -208,6 +268,81 @@ void* thread(void* vargp){
     }
 }
 
+int readItem(char* targetURI, int clientfd){
+    int flag = 0;
+    char buf[MAXLINE];
+
+    P(&mutex);
+    readcnt++;
+    if(readcnt == 1){
+        P(&W);
+    }
+    V(&mutex);
+
+    /***** reading section starts *****/
+    obj_t* cur = cache.head->next;
+    int pos = 1;
+    rio_t rio;
+    Rio_readinitb(&rio, clientfd);
+    while(cur->flag != '@'){
+        if(strcmp(targetURI, cur->uri) == 0){
+            break;
+        }
+
+        cur = cur->next;
+        pos++;
+    }
+
+    flag = cur->flag == '@' ? 0 : pos;
+    char*left = cur->respBody, *right = cur->respBody;
+    while( (right = strstr(left, "\n")) != NULL){
+        strncpy(buf, left, right - left + 1);
+        Rio_writen(clientfd, buf, strlen(buf));
+        left = right + 1;
+    }
+    
+    Rio_writen(clientfd, CR, strlen(CR));
+    Rio_writen(clientfd, cur->respBody, strlen(cur->respBody));
+
+    /***** reading section ends *****/
+    P(&mutex);
+    readcnt--;
+    if(readcnt == 0){
+        V(&W);
+    }
+    V(&mutex);
+
+    return flag;
+}
+
+/*
+ * This function is guarded by Write Lock, thus is thread safe
+ * assume head is the newest part, we evict the last part
+ * if possible
+ */
+void writeToCache(obj_t* obj){
+    /* step1: check current capacity, if full ,delete one */
+    while(obj->respBodyLen + cacheSize > MAX_CACHE_SIZE && cache.head->next != cache.tail){
+        obj_t* last = cache.tail->prev;
+        last->next->prev = last->prev;
+        last->prev->next = last->next;
+
+        last->next = NULL;
+        last->prev = NULL;
+        Free(last);
+    }
+
+    /* step2: add into the cache */
+    //mount the current obj into cache
+    obj->next = cache.head->next;
+    obj->prev = cache.head;
+    cache.head->next->prev = obj;
+    cache.head->next       = obj;
+    cacheSize += obj->respBodyLen;
+}
+
+
+
 int main(int argc, char** argv){
     if(argc != 2){
         unix_error("proxy usage: ./proxy <port number>");
@@ -222,6 +357,14 @@ int main(int argc, char** argv){
     for(i = 0; i < NTHREADS; i++){
         Pthread_create(&tid, NULL, thread, NULL);
     }
+    /* initialize the cache */
+    cacheSize = 0;
+    initializeCache(&cache);
+
+    /* initialize the read / write lock*/
+    readcnt = 0;
+    Sem_init(&mutex, 0, READER_SAME_TIME);
+    Sem_init(&W    , 0, 1);
 
     while(1){
         socklen_t addrLen = sizeof(struct sockaddr_storage);
@@ -230,5 +373,4 @@ int main(int argc, char** argv){
         printf("Accepting Connection from (%s, %s) \n", hostName, port);
         sbuf_insert(&sbuf, connfd);
     }
-
 }
